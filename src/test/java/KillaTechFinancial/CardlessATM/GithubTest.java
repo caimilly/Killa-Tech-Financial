caf0fdaa201b3e82b5e36dbccaa1e9bb57dfdd51
adolison <amdol1@morgan.edu>
2019-08-07 16:08:03 -0400

    testing Github

---
+++ src/test/java/KillaTechFinancial/CardlessATM/GithubTest.java
@@ -0,0 +1,10 @@
+package KillaTechFinancial.CardlessATM;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GithubTest {
+    public void String(){
+        System.out.println("hi");
+    }
+}
